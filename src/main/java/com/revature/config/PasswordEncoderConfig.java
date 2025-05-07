ea17acaf82c3bce45ac545be2f638bbe77da90b7
Daniel Chen <chendg5@gmail.com>
2025-05-07 18:20:59 -0400

    Implemented user login, logout, session info

--- src/main/java/com/revature/config/PasswordEncoderConfig.java
+++ src/main/java/com/revature/config/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package com.revature.CineMatchBackend.config;
+package com.revature.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
